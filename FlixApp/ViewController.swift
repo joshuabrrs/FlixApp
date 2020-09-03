9191c0fbab2885f4e9a7c3fe5820464d1f0e22b5
jgb16 <jgb16@my.fsu.edu>
2020-09-02 22:52:43 -0400

    iOS app that creates movies catalogs

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Joshua Barrios on 9/1/20.
+//  Copyright © 2020 Joshua Barrios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
